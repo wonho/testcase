f19a96dc7dc8198bc82cce3c728f90a2e983efc6
helum <helum@naver.com>
2017-12-01 16:47:37 +0900

    excel

--- src/main/java/com/base/system/config/WebSecurityConfig.java
+++ src/main/java/com/base/system/config/WebSecurityConfig.java
@@ -17,27 +17,27 @@ import com.base.business.auth.UserService;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	@Autowired
-//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		auth
-//			.inMemoryAuthentication()
-//				.withUser("user").password("1111").roles("USER").and()
-//				.withUser("admin").password("2222").roles("USER", "ADMIN");
-//	}
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("1111").roles("USER").and()
+				.withUser("admin").password("2222").roles("USER", "ADMIN");
+	}
 
 	/**
 	 * 직접 userDetailService를 호출하지 않고 AuthenticationProvider를 연결 가능(DaoAuthenticationProvider)
 	 *  authProvider.setUserDetailsService,authProvider.setPasswordEncoder 
 	 *  ex) auth.authenticationProvider(authenticationProvider)
 	 */
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		ShaPasswordEncoder passwordEncoder = new ShaPasswordEncoder();
-		
-//		auth.userDetailsService(userDetailsServiceBean());
-//		auth.userDetailsService(new UserService());
-		auth.userDetailsService(new UserService()).passwordEncoder(passwordEncoder);
-	}
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		ShaPasswordEncoder passwordEncoder = new ShaPasswordEncoder();
+//		
+////		auth.userDetailsService(userDetailsServiceBean());
+////		auth.userDetailsService(new UserService());
+//		auth.userDetailsService(new UserService()).passwordEncoder(passwordEncoder);
+//	}
 	
 	@Bean
 	public PasswordEncoder encoder() {
@@ -53,14 +53,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		    .authorizeRequests()
-//		    .antMatchers("/auth/login.do").hasRole("admin")
-		    .antMatchers("/message/**","/loginProcess.do").permitAll()
+		    .antMatchers("/resource/**").permitAll()
+		    .antMatchers("/**").hasRole("USER")
 		    .anyRequest().authenticated()
 			.and()
 		    .formLogin()
 		        .loginPage("/auth/login.do")
 		        .defaultSuccessUrl("/auth/main.do")
-		        .loginProcessingUrl("/loginProcess.do")
+		        .loginProcessingUrl("/auth/loginProcess.do")
 		        .failureUrl("/auth/login.do")
 		        .permitAll()
 		        .and()
@@ -68,5 +68,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		        .and()
 		    .csrf().disable()
 		    .httpBasic();
+		
+//		http.requiresChannel().antMatchers("/auth/login.do","/auth/loginProcess.do","/auth/logout.do").requiresSecure();
+//        http.portMapper().http(8080).mapsTo(8443);
+//        http.sessionManagement().sessionFixation().none();
 	}
 }
